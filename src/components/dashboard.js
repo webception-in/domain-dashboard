3647ed73104b629532c8fa21f5f81522db9b9821
Vishwas Navada <navadavishwas@hotmail.com>
2019-04-03 09:07:20 +0530

    adding designs

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,73 +1,82 @@
 // App Page
-import React, { Component } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import Ping from "ping.js";
 
-import domains from "../sites.json";
+import userDomains from "../sites.json";
 import { whoIs } from "../config.json";
 
-import Domain from "./domain.js";
+import UserDetails from "./UserDetails";
+import DomainCard from "./DomainCard";
 
-const ping = new Ping();
+const Dashboard = () => {
+  const [domains, setDomain] = useState(userDomains);
+  //   super(props);
+  //   this.state = {
+  //     domains
+  //   };
+  // }
+  // fetchWhoIs(domainName) {
+  //   return axios
+  //     .get(`${whoIs.API_URL}?key=${whoIs.API_KEY}&domain=${domainName}`, {
+  //       crossdomain: true
+  //     })
+  //     .then(res => {
+  //       const { result } = res.data;
+  //       return {
+  //         createdDate: result.created,
+  //         renewedDate: result.changed,
+  //         expiryDate: result.expires
+  //       };
+  //     })
+  //     .catch(err => console.log("Error Fetching Expiry Date", err));
+  // }
+  // checkDomain(domainName) {
+  //   let className = "domain-block ";
+  //   let domainDates = {};
+  //   ping.ping("http://" + domainName, async (err, data) => {
+  //     if (err) {
+  //       className += "site-down";
+  //       domainDates = {
+  //         createdDate: "NA",
+  //         renewedDate: "NA",
+  //         expiryDate: "NA"
+  //       };
+  //       //console.log(domainName+" is unaccessible");
+  //     } else {
+  //       className += "site-up";
+  //       //const domainDates = fetchWhoIs()
+  //       //console.log(domainName+" is up");
+  //     }
+  //   });
+  //   return (
+  //     <Domain
+  //       url={domainName}
+  //       className={className}
+  //       key={domainName}
+  //       dates={domainDates}
+  //     />
+  //   );
+  // }
 
-export default class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      domains
-    };
-  }
-  fetchWhoIs(domainName) {
-    return axios
-      .get(`${whoIs.API_URL}?key=${whoIs.API_KEY}&domain=${domainName}`, {
-        crossdomain: true
-      })
-      .then(res => {
-        const { result } = res.data;
-        return {
-          createdDate: result.created,
-          renewedDate: result.changed,
-          expiryDate: result.expires
-        };
-      })
-      .catch(err => console.log("Error Fetching Expiry Date", err));
-  }
-  checkDomain(domainName) {
-    let className = "domain-block ";
-    let domainDates = {};
-    ping.ping("http://" + domainName, async (err, data) => {
-      if (err) {
-        className += "site-down";
-        domainDates = {
-          createdDate: "NA",
-          renewedDate: "NA",
-          expiryDate: "NA"
-        };
-        //console.log(domainName+" is unaccessible");
-      } else {
-        className += "site-up";
-        //const domainDates = fetchWhoIs()
-        //console.log(domainName+" is up");
-      }
-    });
-    return (
-      <Domain
-        url={domainName}
-        className={className}
-        key={domainName}
-        dates={domainDates}
-      />
-    );
-  }
-  render() {
-    return (
-      <div>
-        <ul className="dashboard">
-          {this.state.domains.map(domain => {
-            return this.checkDomain(domain.url);
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <UserDetails />
+      <ul className="domain-listing">
+        {domains.map(domain => {
+          console.log("TCL: render -> domain", domain);
+
+          return (
+            <DomainCard
+              key={domain.title}
+              title={domain.title}
+              dates={domain.dates}
+              url={domain.url}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import "materialize-css/dist/css/materialize.min.css";
+// import { BrowserRouter, Route } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <div>
+//       <Route exact path="/app" component={Dashboard} />
+//       <Route exact path="/login" component={Login} />
+//     </div>
+//   </BrowserRouter>,
+//   document.getElementById("root")
+// );
+
+ReactDOM.render(<Dashboard />, document.getElementById("root"));
 serviceWorker.register();
